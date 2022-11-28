e0fd7fcec4892e7b2098e9cbb70f42f999e0bd72
Joaquín Ignacio Ossandón Gómez <58451051+Gh0stRaccoon@users.noreply.github.com>
2022-11-28 20:07:44 -0300

    Updates

---
+++ precache-manifest.f8b8a67c7dc1971f4fa737e428c47d55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f1adb81db3d3383900dee831561003b",
+    "url": "/Error404Page/index.html"
+  },
+  {
+    "revision": "3eb0c4230aa66058c53d",
+    "url": "/Error404Page/static/css/main.8b1b487b.chunk.css"
+  },
+  {
+    "revision": "0395c0a0657443388f03",
+    "url": "/Error404Page/static/js/2.94e8b8be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Error404Page/static/js/2.94e8b8be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eb0c4230aa66058c53d",
+    "url": "/Error404Page/static/js/main.28bd2a7b.chunk.js"
+  },
+  {
+    "revision": "fb8c7a18a0e36556a508",
+    "url": "/Error404Page/static/js/runtime-main.5fc9fad5.js"
+  },
+  {
+    "revision": "89e38e339cadd6adb28949899a36a851",
+    "url": "/Error404Page/static/media/exclamation.89e38e33.svg"
+  }
+]);
